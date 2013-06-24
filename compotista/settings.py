1878c157fe0228edab5284e4b9c08fe32d53c812
Laurent Peuch <cortex@worlddomination.be>
2013-06-24 02:11:31 +0200

    [enh] install exported_data app

--- compotista/settings.py
+++ compotista/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'representatives',
     'parltrack_meps',
     'parltrack_meps_to_representatives',
+    'exported_data',
 )
 
 # A sample logging configuration. The only tangible logging
