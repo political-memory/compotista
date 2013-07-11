9fa92c914a350074ba8a0f69a005ef01dc4051ba
Laurent Peuch <cortex@worlddomination.be>
2013-07-11 18:33:55 +0200

    [mod] checksum must be unique

--- exported_data/models.py
+++ exported_data/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class ExportedRevision(models.Model):
     data = models.TextField()
-    checksum = models.CharField(max_length=255)
+    checksum = models.CharField(max_length=255, unique=True)
     creation_datetime = models.DateTimeField()
     last_check_datetime = models.DateTimeField()
