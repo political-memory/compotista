419aa128065de382cf092bf3fa3b60d5db75b7fe
Laurent Peuch <cortex@worlddomination.be>
2013-07-11 18:29:06 +0200

    [enh] I want to have 2 datetime information: creation and last check

--- exported_data/models.py
+++ exported_data/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class ExportedRevision(models.Model):
     data = models.TextField()
     checksum = models.CharField(max_length=255)
-    datetime = models.DateTimeField()
+    creation_datetime = models.DateTimeField()
+    last_check_datetime = models.DateTimeField()
