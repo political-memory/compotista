cbe1310015b57d4e90b930e3c8ad92ce1bee64d8
Laurent Peuch <cortex@worlddomination.be>
2013-07-17 01:24:47 +0200

    [enh] expose checksum of the latest data

--- compotista/urls.py
+++ compotista/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 from exported_data.api import ExportedRevisionResource
-from exported_data.views import get_lastest_data
+from exported_data.views import get_lastest_data, get_lastest_checksum
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,5 +20,6 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^latest/$', get_lastest_data),
+    url(r'^latest/checksum/$', get_lastest_checksum),
     url(r'^api/', include(ExportedRevisionResource().urls)),
 )
