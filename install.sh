1fa9868f1391753e82ff01763eb38c8318d313ba
luxcem <af@laquadrature.net>
2015-04-29 16:00:29 +0200

    fix install

--- install.sh
+++ install.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
 # Copy this install script in a «compotista_project» directory
+branch=dev
+git_compotista=git@git.laquadrature.net:memopol/compotista.git
+git_django_representatives=git@git.laquadrature.net:memopol/compotista_django-representatives.git
+git_import_parltrack=git@git.laquadrature.net:memopol/compotista_import_parltrack.git
 
-python=/usr/bin/python2.7
-git_compotista=git@git.laquadrature.net:luxcem/compotista.git
-git_django_representatives=git@git.laquadrature.net:luxcem/memopol_django-representatives.git
-git_django_parltrack_meps=git@git.laquadrature.net:luxcem/compotista_django-parltrack-meps.git
-git_django_parltrack_meps_to_representatives=git@git.laquadrature.net:luxcem/compotista_django-parltrack-meps-to-representatives.git
-
-virtualenv -p $python ve
+virtualenv --python=python2.7 ve
 
 git clone $git_compotista compotista
 git clone $git_django_representatives django-representatives
-git clone $git_django_parltrack_meps django-parltrack_meps
-git clone $git_django_parltrack_meps_to_representatives django-parltrack_meps_to_representatives
+git clone $git_import_parltrack compotista-import_parltrack
 
 cd compotista
 
-ln -s ../django-representatives/representatives/ .
-ln -s ../django-parltrack_meps/parltrack_meps/ .
-ln -s ../django-parltrack_meps_to_representatives/parltrack_meps_to_representatives/ .
+git checkout $branch
 
 cd ..
 source ve/bin/activate
 
 pip install -r compotista/requirements.txt
-pip install -r django-parltrack_meps/requirements.txt
 
 # python compotista/manage.py syncdb
