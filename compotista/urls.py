3abb44e1c30e8152c2c8250123093cc5b5925efa
luxcem <af@laquadrature.net>
2015-04-29 15:48:51 +0200

    fixes views

--- compotista/urls.py
+++ compotista/urls.py
@@ -7,9 +7,10 @@ from views import home
 
 urlpatterns = patterns('',
     url(r'^$', home),
+    url(r'^latest/checksum/$', get_lastest_checksum),
+    url(r'^latest/(?P<kind>\w+)/checksum/$', get_lastest_checksum),    
     url(r'^latest/$', get_lastest_data),
     url(r'^latest/(?P<kind>\w+)/$', get_lastest_data),
-    url(r'^latest/checksum/$', get_lastest_checksum),
-    url(r'^latest/(?P<kind>\w+)/checksum/$', get_lastest_checksum),
+    
     url(r'^api/', include(ExportedRevisionResource().urls)),
 )
--- exported_data/views.py
+++ exported_data/views.py
@@ -1,12 +1,17 @@
 from .models import ExportedRevision
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 def get_lastest_data(request, kind=None):
-    exported_revision = ExportedRevision.objects.filter(kind=kind).latest('last_check_datetime')
-    return HttpResponse(exported_revision.data)
+    exported_revision = ExportedRevision.objects.filter(kind=kind)
+    if not exported_revision:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
+    return HttpResponse(exported_revision.latest('last_check_datetime').data)
     
 
 def get_lastest_checksum(request, kind=None):
-    print(kind)
-    return HttpResponse(ExportedRevision.objects.latest('last_check_datetime').checksum)
+    exported_revision = ExportedRevision.objects.filter(kind=kind)
+    if not exported_revision:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+    return HttpResponse(exported_revision.latest('last_check_datetime').checksum)
