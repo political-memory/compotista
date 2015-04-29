cb227d52678c23a0d68322abefb716de6370c4f4
luxcem <af@laquadrature.net>
2015-04-29 12:13:45 +0200

    fix home view

--- compotista/urls.py
+++ compotista/urls.py
@@ -1,24 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 
 from exported_data.api import ExportedRevisionResource
 from exported_data.views import get_lastest_data, get_lastest_checksum
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from views import home
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'compotista.views.home', name='home'),
-    # url(r'^compotista/', include('compotista.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^$', home),
     url(r'^latest/$', get_lastest_data),
     url(r'^latest/(?P<kind>\w+)/$', get_lastest_data),
     url(r'^latest/checksum/$', get_lastest_checksum),
