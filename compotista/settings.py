603accccdbaf36a34447d219ff7378f68f1fe025
luxcem <af@laquadrature.net>
2015-06-29 15:48:36 +0200

    update settings loggers

--- compotista/settings.py
+++ compotista/settings.py
@@ -185,6 +185,11 @@ INSTALLED_APPS = (
     'compotista',
 )
 
+
+if DEBUG:
+    INSTALLED_APPS += tuple(get_param('dev_modules'))
+
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
@@ -193,25 +198,36 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s[%(module)s]: %(message)s'
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/compotista-debug.log',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        'compotista': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
         },
-    }
+        'representatives': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    },
 }
 
 
